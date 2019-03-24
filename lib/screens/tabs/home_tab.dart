8f44da3e46bf739bf5559636cbb46032a5e6a837
탁동혁 <tdh8316@naver.com>
2019-03-24 10:21:20 +0900

    Update date format

--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeTab extends StatefulWidget {
   @override
@@ -8,10 +9,44 @@ class HomeTab extends StatefulWidget {
 
 class _HomeTabState extends State<HomeTab> with SingleTickerProviderStateMixin {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    DateTime nowTime = DateTime.now();
+    var dateWidget = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 32, left: 56),
+          child: Text(
+            "${DateFormat("EEEE").format(nowTime)}",
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 2, left: 64),
+          child: Text(
+            "${DateFormat("d").format(nowTime)} ${DateFormat("MMM").format(nowTime)}",
+            style: TextStyle(
+              fontSize: 26,
+              color: Colors.black54,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ],
+    );
     return Scaffold(
-      body: Center(
-        child: Text("Hello"),
+      body: SafeArea(
+        child: dateWidget,
       ),
     );
   }
