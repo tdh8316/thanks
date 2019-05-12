c5a2c183eb19e74dda18b5a1e16fdd38131767fc
탁동혁 <tdh8316@naver.com>
2019-05-12 16:28:28 +0900

    Fix code style

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -40,65 +40,66 @@ class _IndexState extends State<Index> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    var flipBoxBar = FlipBoxBar(
+      navBarHeight: 75.0,
+      animationDuration: Duration(milliseconds: 1500),
+      selectedIndex: _index,
+      onIndexChanged: (index) => setState(() => tabController(index)),
+      items: [
+        FlipBarItem(
+            icon: Icon(Icons.home, color: Colors.white),
+            text: Text("Home", style: TextStyle(color: Colors.white)),
+            frontColor: Colors.deepPurple,
+            backColor: Colors.deepPurpleAccent),
+        FlipBarItem(
+            icon: Icon(Icons.calendar_today, color: Colors.white),
+            text: Text("Calendar", style: TextStyle(color: Colors.white)),
+            frontColor: Colors.lightBlue,
+            backColor: Colors.lightBlueAccent),
+        FlipBarItem(
+            icon: Icon(Icons.pie_chart_outlined, color: Colors.white),
+            text: Text("statistic", style: TextStyle(color: Colors.white)),
+            frontColor: Colors.lightGreen,
+            backColor: Colors.lightGreenAccent),
+        FlipBarItem(
+            icon: Icon(Icons.chrome_reader_mode, color: Colors.white),
+            text: Text("Entries", style: TextStyle(color: Colors.white)),
+            frontColor: Colors.teal,
+            backColor: Colors.tealAccent),
+        FlipBarItem(
+            icon: Icon(Icons.tag_faces, color: Colors.white),
+            text: Text("Profile", style: TextStyle(color: Colors.white)),
+            frontColor: Colors.orange,
+            backColor: Colors.orangeAccent),
+      ],
+    );
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          // TODO: Change to an icon
-          title: Center(
-            child: Text(
-              "Thanks",
-              style: TextStyle(fontSize: 24, color: Colors.black),
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        // TODO: Change to an icon
+        title: Center(
+          child: Text(
+            "Thanks",
+            style: TextStyle(fontSize: 24, color: Colors.black),
           ),
-          elevation: 0,
         ),
-        body: TabBarView(
-          controller: _controller,
-          physics: NeverScrollableScrollPhysics(),
-          children: <Widget>[
-            HomeTab(),
-            CalendarTab(),
-            StatisticTab(),
-            EntriesTab(),
-            ProfileTab(),
-          ],
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 75.0,
-          child: FlipBoxBar(
-            navBarHeight: 75.0,
-            animationDuration: Duration(milliseconds: 1500),
-            selectedIndex: _index,
-            onIndexChanged: (index) => setState(() => tabController(index)),
-            items: [
-              FlipBarItem(
-                  icon: Icon(Icons.home, color: Colors.white),
-                  text: Text("Home", style: TextStyle(color: Colors.white)),
-                  frontColor: Colors.deepPurple,
-                  backColor: Colors.deepPurpleAccent),
-              FlipBarItem(
-                  icon: Icon(Icons.calendar_today, color: Colors.white),
-                  text: Text("Calendar", style: TextStyle(color: Colors.white)),
-                  frontColor: Colors.lightBlue,
-                  backColor: Colors.lightBlueAccent),
-              FlipBarItem(
-                  icon: Icon(Icons.pie_chart_outlined, color: Colors.white),
-                  text:
-                      Text("statistic", style: TextStyle(color: Colors.white)),
-                  frontColor: Colors.lightGreen,
-                  backColor: Colors.lightGreenAccent),
-              FlipBarItem(
-                  icon: Icon(Icons.chrome_reader_mode, color: Colors.white),
-                  text: Text("Entries", style: TextStyle(color: Colors.white)),
-                  frontColor: Colors.teal,
-                  backColor: Colors.tealAccent),
-              FlipBarItem(
-                  icon: Icon(Icons.tag_faces, color: Colors.white),
-                  text: Text("Profile", style: TextStyle(color: Colors.white)),
-                  frontColor: Colors.orange,
-                  backColor: Colors.orangeAccent),
-            ],
-          ),
-        ));
+        elevation: 0,
+      ),
+      body: TabBarView(
+        controller: _controller,
+        physics: NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          HomeTab(),
+          CalendarTab(),
+          StatisticTab(),
+          EntriesTab(),
+          ProfileTab(),
+        ],
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 75.0,
+        child: flipBoxBar,
+      ),
+    );
   }
 }
