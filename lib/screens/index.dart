322eda54cf4a903211cfc56ca6ac8ed34132081d
탁동혁 <tdh8316@naver.com>
2019-06-15 18:45:35 +0900

    Update UI code

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -4,9 +4,9 @@ import 'package:intl/intl.dart';
 import 'package:thanks/core/i18n.dart';
 
 class Index extends StatefulWidget {
-  Widget dateWidget() {
+  Widget bottomCardView() {
     DateTime date = DateTime.now();
-    return Column(
+    Widget dateWidget = Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.end,
       children: <Widget>[
@@ -34,104 +34,98 @@ class Index extends StatefulWidget {
         ),
       ],
     );
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40),
+              topRight: Radius.circular(40),
+            ),
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(bottom: 32),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: <Color>[
+                    Color.fromARGB(255, 115, 75, 245),
+                    Color.fromARGB(255, 185, 90, 250),
+                  ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  dateWidget,
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   State<StatefulWidget> createState() => IndexState();
+
+  SliverAppBar sliverAppBar(bool innerBoxIsScrolled) {
+    return SliverAppBar(
+      forceElevated: innerBoxIsScrolled,
+      backgroundColor: Color.fromARGB(255, 36, 39, 52),
+      iconTheme: IconThemeData(color: Colors.white),
+      expandedHeight: 200,
+      floating: true,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        collapseMode: CollapseMode.pin,
+        centerTitle: true,
+        title: Text(
+          "Thanks",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: "Potra",
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class IndexState extends State<Index> with SingleTickerProviderStateMixin {
-  static int _index = 0;
-  TabController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = TabController(length: 5, vsync: this);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     super.dispose();
-  }
-
-  setIndex(int index) {
-    _index = index;
-    _controller.animateTo(_index, curve: Curves.easeOut);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomPadding: true,
+      resizeToAvoidBottomInset: true,
       backgroundColor: Color.fromARGB(255, 36, 39, 52),
-      /*appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 36, 39, 52),
-        // TODO: Change to an icon
-        title: Center(
-          child: Text(
-            "Thanks",
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: "Potra",
-            ),
-          ),
-        ),
-        elevation: 0,
-      ),*/
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              backgroundColor: Color.fromARGB(255, 36, 39, 52),
-              expandedHeight: 200,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(
-                  "Thanks",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: "Potra",
-                  ),
+      body: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                child: SliverSafeArea(
+                  sliver: widget.sliverAppBar(innerBoxIsScrolled),
                 ),
               ),
-            ),
-          ];
-        },
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-                child: Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.only(bottom: 32),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: <Color>[
-                        Color.fromARGB(255, 115, 75, 245),
-                        Color.fromARGB(255, 185, 90, 250),
-                      ],
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      widget.dateWidget(),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ];
+          },
+          body: widget.bottomCardView(),
         ),
       ),
     );
