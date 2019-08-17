95fad0c91a3db41a215dee915f6902154479173e
탁동혁 <tdh8316@naver.com>
2019-08-17 23:22:15 +0900

    Add a new page `Settings`

---
+++ lib/pages/settings.dart
@@ -0,0 +1,160 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:thanks/styles/default.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> with TickerProviderStateMixin {
+  PageController pageController;
+  int currentPage = 0;
+  double page = 0.0;
+  double viewPortFraction = .4;
+
+  @override
+  void initState() {
+    pageController = PageController(viewportFraction: viewPortFraction);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Spacer(),
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      DefaultStyle.primary1,
+                      DefaultStyle.primary2,
+                    ],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                ),
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (ScrollNotification notification) {
+                    setState(() {
+                      page = pageController.page;
+                    });
+                    return true;
+                  },
+                  child: PageView.builder(
+                    controller: pageController,
+                    onPageChanged: (pos) {
+                      setState(() {
+                        currentPage = pos;
+                      });
+                    },
+                    itemCount: 4,
+                    physics: BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      // print(max(.7, (1 - (index - page).abs()) + viewPortFraction)*60);
+                      return _buildColorsWidgets()[index](currentPage == index);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Function> _buildColorsWidgets() => <Function>[
+        (bool isPrimary) => FlatButton(
+              child: Container(
+                height: isPrimary ? 100 : 64,
+                width: isPrimary ? 100 : 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                  gradient: LinearGradient(
+                    colors: defaultColors(),
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedTheme = ColorSchemes.default_;
+                });
+              },
+            ),
+        (bool isPrimary) => FlatButton(
+              child: Container(
+                height: isPrimary ? 100 : 64,
+                width: isPrimary ? 100 : 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                  gradient: LinearGradient(
+                    colors: nightFadeColors(),
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedTheme = ColorSchemes.nightFade;
+                });
+              },
+            ),
+        (bool isPrimary) => FlatButton(
+              child: Container(
+                height: isPrimary ? 100 : 64,
+                width: isPrimary ? 100 : 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                  gradient: LinearGradient(
+                    colors: deepBlueColors(),
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedTheme = ColorSchemes.deepBlue;
+                });
+              },
+            ),
+        (bool isPrimary) => FlatButton(
+              child: Container(
+                height: isPrimary ? 100 : 64,
+                width: isPrimary ? 100 : 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white),
+                  gradient: LinearGradient(
+                    colors: grapeColors(),
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topLeft,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedTheme = ColorSchemes.grape;
+                });
+              },
+            ),
+      ];
+}
